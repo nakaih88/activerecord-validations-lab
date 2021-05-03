880c60fa0ea972492e5e44bdd70c12e073c9e0ad
Nakai <nakai.hamlin18@gmail.com>
2021-05-03 17:38:23 -0400

    commit tree ab6b1af3ed709a3a12d3c204a56d1492ba423f7d

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,18 @@
 class Post < ActiveRecord::Base
+    validates :title, presence: true
+
+    validates :content,length: {minimum: 250}
+
+    validates :summary, length: {maximum: 250}
+
+    validates :category, inclusion: { in: %w(Fiction Non-Fiction) }
+
+    validate :clickbait?
+
+    def clickbait?
+        clickbait = ["Won't Believe", "Secret", "Top[number]", "Guess"]
+        if title && !clickbait.any? { |w| title.include?(w) }
+          errors.add(:title, " needs to be more clickbait-y")
+        end
+      end
 end
